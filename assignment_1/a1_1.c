43490e8a3740dac62060fe241e9f447c7bd23f07
Gaurav Juvekar <gauravjuvekar@gmail.com>
2016-09-09 13:22:13 +0530

    Use files in a11

--- assignment_1/a1_1.c
+++ assignment_1/a1_1.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 	char name[1024];
 	char number[1024];
-	while (fscanf(stdin, "%s%s", name, number) != EOF) {
-		fprintf(stdout, "%-*s%*s\n", 40, name, 80, number);
+
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
+		exit(1);
 	}
+	FILE *input = fopen(argv[1], "r");
+	FILE *output = fopen(argv[2], "w");
+
+	if (input == NULL || output == NULL) {
+		fprintf(stderr, "Unable to open input or output file\n");
+		exit(1);
+	}
+
+	while (fscanf(input, "%s%s", name, number) != EOF) {
+		fprintf(output, "%-*s%*s\n", 40, name, 80, number);
+	}
+
+	fclose(input);
+	fclose(output);
+
 	return 0;
 }
