f96ac8dd7752d1d03e1b90056797701c768296ed
Gaurav Juvekar <gauravjuvekar@gmail.com>
2016-10-24 11:06:21 +0530

    Dont send the process in interrupt if it isnt the arrival time of the process

--- assignment_4/base.py
+++ assignment_4/base.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plot
 import matplotlib
 import numpy
 import itertools
+from collections import deque
 
 
 class Process(object):
@@ -70,35 +71,43 @@ class Scheduler(object):
 
 
 def run(scheduler_class):
-    processes = [
+    processes = deque([
         (0, Process(pid=1, burst_time=4, priority=4)),
         (1, Process(pid=2, burst_time=5, priority=0)),
         (2, Process(pid=3, burst_time=2, priority=2)),
         (3, Process(pid=4, burst_time=3, priority=1)),
-        (4, Process(pid=5, burst_time=6, priority=3))]
+        (20, Process(pid=5, burst_time=6, priority=3))])
 
     scheduler = scheduler_class()
 
-    processes = iter(processes)
     try:
-        next_process = processes.__next__()
-    except StopIteration:
+        next_process = processes.popleft()
+    except IndexError:
         next_process = None
         next_interrupt = None
     else:
         next_interrupt = next_process[0]
-    while next_interrupt is not None:
+    while next_interrupt is not None or next_process is not None:
         if next_process is None:
             try:
-                next_process = processes.__next__()
-            except StopIteration:
+                next_process = processes.popleft()
+            except IndexError:
                 next_process = None
         if next_process is not None:
-            next_interrupt = min(next_interrupt, next_process[0])
-            next_process = next_process[1]
-
-        next_interrupt = scheduler.interrupt(time=next_interrupt,
-                                             process=next_process)
-        next_process = None
+            if next_interrupt is None:
+                next_interrupt = next_process[0]
+            else:
+                next_interrupt = min(next_interrupt, next_process[0])
+        if next_process is not None:
+            if next_interrupt == next_process[0]:
+                next_interrupt = scheduler.interrupt(time=next_interrupt,
+                                                     process=next_process[1])
+                next_process = None
+            else:
+                next_interrupt = scheduler.interrupt(time=next_interrupt,
+                                                     process=None)
+        else:
+            next_interrupt = scheduler.interrupt(time=next_interrupt,
+                                                 process=None)
 
     scheduler.print()
